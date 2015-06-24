b353170efdbf31cf84d837d2571d0937c33e62df
wolandscat <thomas.beale@oceaninformatics.com>
2015-06-24 15:44:06 +0100

    Re-arrange CSS resources; improve execution script.

--- source/AOM2/generate.sh
+++ source/AOM2/generate.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-asciidoctor -a localyear=`date +%G` --out-file ../../publishing/HTML/AOM2.html AOM2.adoc
+#!/usr/bin/bash
+asciidoctor -a localyear=`date +%G` -a stylesdir=../../css -a stylesheet=openehr.css AOM2.adoc
+
+# In theory, Use the following as a production command; note that it doesn't copy over all the needed images. Needs an extension to do this
+#asciidoctor -a localyear=`date +%G` -a stylesdir=../css -a stylesheet=openehr.css --out-file ../../publishing/HTML/AOM2.html AOM2.adoc
+
+# use the following to experiment with CSS changes; requires the Git repo http://github.com/asciidoctor/asciidoctor-stylesheet-factory.git to 
+# be cloned as a sibling of the openEHR specifications-XX Git repos
+#asciidoctor -a localyear=`date +%G` -a stylesdir=../../../asciidoctor-stylesheet-factory/stylesheets -a stylesheet=openehr.css AOM2.adoc
+#asciidoctor -a localyear=`date +%G` -a stylesdir=../../../asciidoctor-stylesheet-factory/stylesheets AOM2.adoc
